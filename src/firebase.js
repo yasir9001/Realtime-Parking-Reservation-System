ec07954a19b413b0defa3d9ae7028d810cb9aa60
yasir9001 <43116578+yasir9001@users.noreply.github.com>
2019-03-28 01:35:18 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyCLqYta-kMRm8CswE67DeJYJtqMJYjSQD0",
-    authDomain: "project-level-zero.firebaseapp.com",
-    databaseURL: "https://project-level-zero.firebaseio.com",
-    projectId: "project-level-zero",
-    storageBucket: "project-level-zero.appspot.com",
-    messagingSenderId: "118685174159"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
   firebase.initializeApp(config);
   export default firebase;
